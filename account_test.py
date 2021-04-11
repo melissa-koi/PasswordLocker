9341443d919ebe7d3af330d3b9fae9a95bfd9d76
melissa-koi <melissawangui3@gmail.com>
2021-04-11 17:46:26 +0300

    add account test file

---
+++ account_test.py
@@ -0,0 +1,36 @@
+import unittest
+from locker import Account
+import pyperclip
+
+class TestAccount(unittest.TestCase):
+
+    def setUp(self):
+        self.new_account = Account("test_username", "test_password1")
+
+    def tearDown(self):
+       Account.account_details = []
+
+    def test_init(self):
+        self.assertEqual(self.new_account.username, "test_username")
+        self.assertEqual(self.new_account.password, "test_password1")
+
+    def test_save_account(self):
+        self.new_account.save_account()
+        self.assertEqual(len(Account.account_details),1)
+
+    def test_save_multiple_accounts(self):
+        self.new_account.save_account()
+        test_account = Account("username2", "password2")
+        test_account.save_account()
+
+        self.assertEqual(len(Account.account_details),2)
+
+    def test_delete_account(self):
+        self.new_account.save_account()
+        test_account = Account("username2", "password2")
+        test_account.save_account()
+
+        self.new_account.delete_account()
+        self.assertEqual(len(Account.account_details),1)
+
+
