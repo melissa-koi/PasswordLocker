4c17788f8f676769ee9647bde6a9f6c9c0150f53
melissa-koi <melissawangui3@gmail.com>
2021-04-11 18:17:24 +0300

    rename main.py to run.py and made it executable

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from locker import User
+from locker import Account
+
+
+
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
