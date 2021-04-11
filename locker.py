ef40d4d610e386f6210490725eb45ec628e67464
melissa-koi <melissawangui3@gmail.com>
2021-04-11 16:23:44 +0300

    create three files

---
+++ locker.py
@@ -0,0 +1,9 @@
+import pyperclip
+
+class Account:
+
+    account_details = []
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
---
+++ locker_test.py
@@ -0,0 +1,7 @@
+import unittest
+from locker import Account
+import pyperclip
+
+class TestAccount(unittest.TestCase):
+
+    
