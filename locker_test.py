72a652fae6ab3e914e17a8b27f5d6ce3d722d685
melissa-koi <melissawangui3@gmail.com>
2021-04-11 17:46:01 +0300

    add tests for saving and deleting the accounts

--- locker_test.py
+++ locker_test.py
@@ -6,32 +6,32 @@ class TestAccount(unittest.TestCase):
 
 #UserAccount
     def setUp(self):
-        self.new_account = User("test_username", "test_password1")
+        self.new_user = User("test_username", "test_password1")
 
     def tearDown(self):
         User.user_details = []
 
     def test_init(self):
-        self.assertEqual(self.new_account.username, "test_username")
-        self.assertEqual(self.new_account.password, "test_password1")
+        self.assertEqual(self.new_user.username, "test_username")
+        self.assertEqual(self.new_user.password, "test_password1")
 
     def test_save_account(self):
-        self.new_account.save_account()
+        self.new_user.save_account()
         self.assertEqual(len(User.user_details),1)
 
     def test_save_multiple_accounts(self):
-        self.new_account.save_account()
+        self.new_user.save_account()
         test_account = User("username2", "password2")
         test_account.save_account()
 
         self.assertEqual(len(User.user_details),2)
 
     def test_delete_account(self):
-        self.new_account.save_account()
+        self.new_user.save_account()
         test_account = User("username2", "password2")
         test_account.save_account()
 
-        self.new_account.delete_account()
+        self.new_user.delete_account()
         self.assertEqual(len(User.user_details),1)
 
 #AccountDetails
